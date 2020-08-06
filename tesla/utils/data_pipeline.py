ee09089ae4c308661b3a833572c350c6801ff0f0
KnightZhang <knight.625@live.com>
2020-08-06 11:50:44 +0800

    finish covert function for CONLL2000

--- tesla/utils/data_pipeline.py
+++ tesla/utils/data_pipeline.py
@@ -17,6 +17,12 @@
 # limitations under the License.
 # ==============================================================================
 
+import codecs
+import pickle
+import functools
+from pathlib import Path
+MAIN_PATH = Path(__file__).absolute().parent.parent.parent
+
 def createBatchIndex(total_length, batch_size):
   """Provide indices for the batch data."""
   batch_number = total_length // batch_size
@@ -24,3 +30,47 @@ def createBatchIndex(total_length, batch_size):
 
   for i in range(batch_number):
     yield (i*batch_number, i*batch_number+batch_number)
+
+def _createDefaultSavePath(func):
+  """Decorator for creating the binary file name."""
+  @functools.wraps(func)
+  def _createDefaultSavePathWrapper(dataset_name, data_path, save_path=None):
+    if save_path is not None:
+      func(dataset_name, data_path, save_path)
+    else:
+      data_path = Path(data_path)
+      path_parent = data_path.parent
+      data_name = data_path.stem + '.bin'
+      save_path = path_parent / data_name
+      func(dataset_name, data_path, save_path)
+    print('"{}" has been converted successfully to "{}".'.format(data_path.name, save_path))
+  
+  return _createDefaultSavePathWrapper
+
+@_createDefaultSavePath
+def convertToBinaryData(dataset_name, data_path, save_path=None):
+  """Convert the txt format file to the binary file."""
+  if dataset_name == 'conll2000':
+    with codecs.open(data_path, 'r', 'utf-8') as file:
+      datas = []
+      one_data = []
+      for line in file:
+        line = line.strip()
+        if len(line) != 0:
+          one_data.append(line)
+        else:
+          if len(one_data) != 0:
+            datas.append(one_data)
+            one_data = []
+  else:
+    print('Do not support this dataset: "{}".'.format(dataset_name))
+    raise ValueError
+  
+  print('Data total length: {}.'.format(len(datas)))
+  with codecs.open(save_path, 'wb') as file:
+    pickle.dump(datas, file)
+
+if __name__ == '__main__':
+  ### Convert CONLL2000 Dataset ###
+  conll2000_data_path = MAIN_PATH / 'datasets/CONLL2000/train.txt'
+  convertToBinaryData('conll2000', conll2000_data_path)
