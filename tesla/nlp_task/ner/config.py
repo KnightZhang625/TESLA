3bd17a71ec7ee8edfb4c259d5f949716bd58e368
KnightZhang <knight.625@live.com>
2020-08-07 17:06:58 +0800

    configuration file

--- tesla/nlp_task/ner/config.py
+++ tesla/nlp_task/ner/config.py
@@ -1,4 +1,40 @@
 # cofing:utf-8
 # configuration for the NER
 
+import sys
+from pathlib import Path
+MAIN_PATH = Path(__file__).absolute().parent.parent.parent.parent
+sys.path.append(str(MAIN_PATH))
+
+from tesla.utils.functools import NoNewAttrs
+
 SAVE_MODEL_PATH = './models'
+LEARNING_RATE = 1e-4
+LEARNING_RATE_LIMIT = 1e-4
+TRAIN_STEPS = 100
+
+SAVE_STEPS = 10
+BATCH_SZIE = 30
+
+DATA_PATH = MAIN_PATH / 'datasets/CONLL2000/train.bin'
+
+class Config(NoNewAttrs):
+  enable_char_embedding=True
+  char_size=28
+  char_embedding_size=16
+  padding_seq_length=50
+  vocab_size=19124
+  embedding_size=320
+  window_size=[2, 3, 4]
+  pool_size=[14, 13, 12]
+  filter_number=2
+  num_layers=4
+  cell_type='gru'
+  forget_bias=True
+  hidden_size=320
+  num_classes=46
+  initialize_range=1e-2
+  dropout=0.1
+
+model_config = Config()
+print(model_config.initialize_range)
