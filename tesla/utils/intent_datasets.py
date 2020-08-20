c1bba3a9ad6fa2160e48da4a446695016354c7f8
KnightZhang <knight.625@live.com>
2020-08-20 15:00:43 +0800

    finish data pipeline

--- tesla/utils/intent_datasets.py
+++ tesla/utils/intent_datasets.py
@@ -16,15 +16,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import gc
 import sys
+import copy
 import json
 import codecs
+import random
 import functools
+import tensorflow as tf
+tf.enable_eager_execution()
 from pathlib import Path
 MAIN_PATH = Path(__file__).absolute().parent.parent.parent
 sys.path.append(str(MAIN_PATH))
 
-from tesla.utils.data_pipeline import convertStrToIdx
+from tesla.utils.data_pipeline import convertStrToIdx, createBatchIndex, padding_func
 
 class IntentText(object):
   """Base Object for Intent Extraction Dataset."""
@@ -69,6 +74,13 @@ class IntentText(object):
 
     assert len(texts_idx) == len(tags_idx) == len(indents_idx) == len(chars_idx)
 
+    train_texts_idx, train_tags_idx, train_indents_idx, train_chars_idx = \
+      texts_idx[:train_size], tags_idx[:train_size], indents_idx[:train_size], chars_idx[:train_size]
+    test_texts_idx, test_tags_idx, test_indents_idx, test_chars_idx = \
+      texts_idx[-test_size:], tags_idx[-test_size:], indents_idx[-test_size:], chars_idx[-test_size:]
+
+    return zip(train_texts_idx, train_tags_idx, train_indents_idx, train_chars_idx), \
+            zip(test_texts_idx, test_tags_idx, test_indents_idx, test_chars_idx)
 
   def makeWordDict(self, data):
     vocab_idx = {}
@@ -85,6 +97,57 @@ class IntentText(object):
     
     return vocab_idx, idx_vocab
   
+  def generateData(self, datasets, batch_size=32):
+    """Generator for Tensorflow Dataset"""
+    # shuffle the data
+    datasets_copy = list(copy.deepcopy(datasets))
+    random.shuffle(datasets_copy)
+
+    texts, tags, indents, chars = zip(*datasets_copy)
+    data_length = len(texts)
+    for s, e in createBatchIndex(data_length, batch_size):
+      batch_texts = texts[s : e]
+      input_texts_length = [len(sent) for sent in batch_texts]
+      max_input_texts_length = max(input_texts_length)
+      padding_func_texts = functools.partial(
+        padding_func, max_length=max_input_texts_length, pad_idx=self.word_idx['<pad>'])
+      input_texts = list(map(padding_func_texts, batch_texts))
+
+      batch_tags = tags[s : e]
+      batch_tags_length = [len(tag) for tag in batch_tags]
+      padding_func_tag = functools.partial(
+        padding_func, max_length=max(batch_tags_length), pad_idx=self.tag_idx['<pad>'])
+      output_tags = list(map(padding_func_tag, batch_tags))
+ 
+      output_indents = indents[s : e]
+      
+      batch_chars = chars[s : e]
+      input_chars_length = [[len(char) for char in sent] for sent in batch_chars]
+      padding_func_charLength = functools.partial(
+        padding_func, max_length=max_input_texts_length, pad_idx=0)
+      input_chars_length = list(map(padding_func_charLength, input_chars_length))
+
+      max_char_length = max([max(sent_length) for sent_length in input_chars_length])
+      padding_func_char = functools.partial(
+        padding_func, max_length=max_char_length, pad_idx=self.char_idx['<pad>'])
+      input_chars = [list(map(padding_func_char, sent)) for sent in batch_chars]
+
+      padding_func_char_sent = functools.partial(
+        padding_func, max_length=max_input_texts_length,
+        pad_idx=[self.char_idx['<pad>'] for _ in range(max_char_length)])
+      input_chars = list(map(padding_func_char_sent, input_chars))
+      
+      input_data = {'input_texts': input_texts,
+                    'input_texts_length': input_texts_length,
+                    'input_chars': input_chars,
+                    'input_chars_length': input_chars_length}
+      output_data = {'output_tags': output_tags,
+                     'output_indents': output_indents}
+      yield (input_data, output_data)
+
+    del datasets_copy
+    gc.collect()
+
 class SNIPS(IntentText):
   """
   SNIPS dataset class
@@ -127,7 +190,7 @@ class SNIPS(IntentText):
     self.dir_path = dir_path
     train_data_raw, test_data_raw = self._loadDatasets()
     super(SNIPS, self).__init__(sentence_length=sentence_length, word_length=word_length)
-    self.processData(train_data_raw, test_data_raw)
+    self.train_datas, self.test_datas = self.processData(train_data_raw, test_data_raw)
 
   def _loadDatasets(self):
     train_data = self._loadIntents(self.train_files)
@@ -156,6 +219,9 @@ class SNIPS(IntentText):
       tags_list = []
       for te in sent:
         token = te['text'].strip().split(' ')
+        # ignore the null
+        if len(token[0]) == 0:
+          continue
         tokens_list += token
         entity = te.get('entity', None)
         if entity is not None:
@@ -164,6 +230,31 @@ class SNIPS(IntentText):
           tags_list += ['O'] * len(token)
       sentences.append((tokens_list, tags_list))
     return sentences
+  
+  def trainInputFn(self, is_train, batch_size, steps):
+    output_types = {'input_texts': tf.int32,
+                    'input_texts_length': tf.int32,
+                    'input_chars': tf.int32,
+                    'input_chars_length': tf.int32}
+    output_shapes = {'input_texts': [None, None],
+                     'input_texts_length': [None],
+                     'input_chars': [None, None, None],
+                     'input_chars_length': [None, None]}
+    label_types = {'output_tags': tf.int32,
+                    'output_indents': tf.int32}
+    label_shapes = {'output_tags': [None, None],
+                    'output_indents': [None]}
+    
+    data_generator = functools.partial(self.generateData, 
+                        datasets=self.train_datas if is_train else self.test_datas,
+                        batch_size=batch_size)
+    dataset = tf.data.Dataset.from_generator(
+      data_generator,
+      output_types=(output_types, label_types),
+      output_shapes=(output_shapes, label_shapes))
+    dataset = dataset.repeat(steps)
+
+    return dataset
   
   @staticmethod
   def _createTags(tag, length):
@@ -175,3 +266,10 @@ class SNIPS(IntentText):
 
 if __name__ == '__main__':
   snips = SNIPS()
+  
+  # for data in snips.generateData(snips.train_datas, batch_size=2):
+  #   print(data)
+  #   input()
+
+  for data in snips.trainInputFn(True, 100, 2):
+    print(data)
